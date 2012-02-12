cc932e6232576aa131cf319ab5ad70d828531890
rtnk29 <rtnk29@ubuntu.(none)>
2012-02-11 18:37:04 -0800

    add open, read, release, write, ioctl and check minor number

--- cdata.c
+++ cdata.c
@@ -17,19 +17,58 @@
 
 static int cdata_open(struct inode *inode, struct file *filp)
 {
-	return 0;
+    printk(KERN_INFO "CDATA: in open\n");
+    if(MINOR(inode->i_rdev) != 0) {
+        printk(KERN_INFO "CDATA: open error number\n");
+    }
+    printk(KERN_INFO "CDATA: minor %d\n", MINOR(inode->i_rdev));
+    return 0;
 }
 
-static struct file_operations cdata_fops = {	
-	open:		cdata_open,
+ssize_t cdata_write(struct file *filp, const char *buf, size_t size,
+    loff_t *off)
+{
+    return 0;
+}
+
+int cdata_close(struct inode *inode, struct file *filp)
+{
+    return 0;
+}
+
+ssize_t cdata_read(struct file *filp, char *buf, size_t size,
+    loff_t *off)
+{
+    return 0;
+}
+
+int cdata_ioctl(struct file *filp, int cmd, struct file_lock* lock)
+{
+    return 0;
+}
+
+static struct file_operations cdata_fops = {
+    owner: THIS_MODULE,
+    open: cdata_open,
+    release: cdata_close,
+    write: cdata_write,
+    read: cdata_read,
+    ioctl: cdata_ioctl,
 };
 
 int cdata_init_module(void)
 {
+    if (register_chrdev(121, "cdata", &cdata_fops) < 0) {
+        printk(KERN_INFO "CDATA: can't register driver\n");
+        return -1;
+    }
+    printk(KERN_INFO "CDATA: Init\n");
+    return 0;
 }
 
 void cdata_cleanup_module(void)
 {
+    unregister_chrdev(121, "cdata");
 }
 
 module_init(cdata_init_module);
