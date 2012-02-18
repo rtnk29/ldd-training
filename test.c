606f1b7a335465cf41213bdca8455d1fdef48a07
rtnk29 <rtnk29@ubuntu.(none)>
2012-02-17 23:51:57 -0800

    exercise4: try to use "offset"

--- test.c
+++ test.c
@@ -17,8 +17,9 @@ int main(void)
 
     fd = open("/dev/cdata", O_RDWR);
     ioctl(fd, CDATA_CLEAR, &i);
-    write(fd, pix, 4); // write 1 dot
-    write(fd, pix, 4); // write 1 dot
+    while(1) {
+        write(fd, pix, 4); // write 1 dot
+    }    
 
     close(fd);
 }
