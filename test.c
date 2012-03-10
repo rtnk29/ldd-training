826be4c85b4d09456dfcf9e6babcc98fc51c7bd4
rtnk29 <rtnk29@ubuntu.(none)>
2012-03-10 11:20:47 -0800

    add mmap system call, but no vma show in /proc/pid/maps

--- test.c
+++ test.c
@@ -14,12 +14,25 @@ int main(void)
     int fd;
     int i= 10000;    
     char pix[4] = {0x00, 0xff, 0x00, 0xff};
+    char pix2[4] = {0xff, 0xff, 0xff, 0xff};
+    //pid_t pid;
 
+    //pid=fork();
     fd = open("/dev/cdata", O_RDWR);
-    ioctl(fd, CDATA_CLEAR, &i);
-    while(1) {
-        write(fd, pix, 4); // write 1 dot
-    }    
-
+/*
+    //pid=fork();
+    if(pid==0){
+        ioctl(fd, CDATA_CLEAR, &i);
+        while(1) {
+            write(fd, pix, 4); // write 1 dot
+        }
+    } else {
+        while(1) {
+            write(fd, pix2, 4); // write 1 dot
+        }        
+    }
+*/
+    mmap(0, 1024, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+    sleep(15);
     close(fd);
 }
